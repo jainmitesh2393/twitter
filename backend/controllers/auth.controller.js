8cd0aaa33c78a82a6b5cd3953e1c829c5b3e7ab1
Mitesh jain <jainmitesh2393@gmail.com>
2024-08-21 10:08:23 +0530

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+  res.json({ data: "you hit the signup end point" });
+};
+export const login = async (req, res) => {
+  res.json({ data: "you hit the login end point" });
+};
+export const logout = async (req, res) => {
+  res.json({ data: "you hit the login end point" });
+};
